9fc4f1e62303ef9e0a4f14a32536e40e72aebe37
Antonio <antonio.alexander.a10@gmail.com>
2020-12-14 21:25:12 -0300

    Testeo de métodos(#2)

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,11 @@ public class Main {
 	public static void main(String[] args) {
 		Patrones patrones = new Patrones();
 		
-		patrones.patron1(5);
+		patrones.Patron1(5);
+		patrones.Patron2(5);
+		patrones.Patron3(5);
+		patrones.Patron4(5);
+
 
 	}
 
