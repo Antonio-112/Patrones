6462ee97fb2f76e03c10de071648df35f3ccc3c6
Antonio <antonio.alexander.a10@gmail.com>
2020-12-14 21:18:51 -0300

    Creacion de metodo 2(#2)

--- src/util/Patrones.java
+++ src/util/Patrones.java
@@ -2,7 +2,7 @@ package util;
 
 public class Patrones {
 	
-	public void patron1(int n) {	
+	public void Patron1(int n) {	
 		for(int i=0;i<n;i++) {
 			System.out.println();
 			for(int j =0;j<n;j++) {
@@ -11,6 +11,20 @@ public class Patrones {
 				else System.out.printf(" ");
 			}
 		}
+		System.out.println();
+	}
+	
+	public void Patron2(int n) {
+		for(int i=0;i<n;i++) System.out.printf("*"); 
+		System.out.println();
+		for(int i=n;i>2;i--) {
+			for(int j=0;j<n;j++) {
+				if(j==i-2) System.out.printf("*");
+				else System.out.printf(" ");
+			}
+			System.out.println("");
+		}
+		for(int i=0;i<n;i++) System.out.printf("*");
 		System.out.println();
 	}
 
