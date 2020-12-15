f6409336dfcff3a5fc40ce4c06550106cac43299
Antonio <antonio.alexander.a10@gmail.com>
2020-12-14 21:10:14 -0300

    Testeo de método(#1)

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import util.Patrones;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Patrones patrones = new Patrones();
+		
+		patrones.patron1(5);
+
+	}
+
+}
